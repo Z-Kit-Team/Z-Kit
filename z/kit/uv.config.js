c29fec27e73212b0e0be9ca89c084734c7d6c105
above <160554522+Abovescore@users.noreply.github.com>
2025-03-26 20:18:28 -0500

    if this doesnt work ill just chane kit into kill

--- z/kit/uv.config.js
+++ z/kit/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/z/-kit/',
+    prefix: '/z/kit/',
     bare:'https://goto.zkit.above.gay/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
